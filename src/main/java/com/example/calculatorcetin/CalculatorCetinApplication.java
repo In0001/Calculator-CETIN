1fea879508ee4b1e2991211cb333db7227aa174c
Inga <in264659@yandex.ru>
2023-03-06 21:36:25 +0300

    Добавление файлов проекта

---
+++ src/main/java/com/example/calculatorcetin/CalculatorCetinApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorcetin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCetinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorCetinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorcetin/CalculatorCetinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorcetin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorCetinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
